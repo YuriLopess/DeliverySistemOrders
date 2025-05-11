6e0eb71efd583e08df2e2c451018a86b3aa5d2f9
YuriLopess <costalopesyuri@gmail.com>
2025-05-11 11:06:38 -0300

    initial commit

---
+++ src/test/java/io/github/YuriLopess/DeliverySystemOrders/DeliverySystemOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.YuriLopess.DeliverySystemOrders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverySystemOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
